b86fc983387b39f8db119269398298fb96e74dcd
deoxxa <deoxxa@fknsrs.biz>
2013-09-20 02:09:49 +1000

    wip experiment of handler usage

--- dump.js
+++ dump.js
@@ -2,7 +2,29 @@
 
 var WSDL = require("./");
 
-WSDL.load(process.argv[2], function(err, wsdl) {
+var options = {
+  bindingHandlers: [function(binding, element) {
+    var soapBindings = element.getElementsByTagNameNS("http://schemas.xmlsoap.org/wsdl/soap/", "binding");
+
+    if (soapBindings.length === 1) {
+      binding.soapBinding = {
+        style: soapBindings[0].getAttribute("style"),
+        transport: soapBindings[0].getAttribute("transport"),
+      };
+    }
+  }],
+  operationHandlers: [function(operation, element) {
+    var soapOperations = element.getElementsByTagNameNS("http://schemas.xmlsoap.org/wsdl/soap/", "operation");
+
+    if (soapOperations.length === 1) {
+      operation.soapOperations = {
+        soapAction: soapOperations[0].getAttribute("soapAction"),
+      };
+    }
+  }],
+};
+
+WSDL.load(options, process.argv[2], function(err, wsdl) {
   if (err) {
     return console.warn(err);
   }
